b96a68f09448a0a81a0eeae941d82190a4b54d11
boufu <darylchee1234@gmail.com>
2025-04-29 15:12:28 +0800

    Update Servo Motor.py

--- Servo Motor.py
+++ Servo Motor.py
@@ -7,18 +7,47 @@ from time import sleep
 # min_pulse_width and max_pulse_width may need to be adjusted for your servo
 servo = AngularServo(13, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
-servo.angle = 0 # set servo.angle to default position
+servo.angle = 90 # set servo.angle to default position
+sleep(1)
+steps = 30
+increment = 30 / steps
 
 # Function to set the servo angle
-def set_angle(angle):
-    servo.angle = angle
-    sleep(1)
+def set_angle():
+        try:
+                for i in range(3):
+                        position = 80
+                        counter = 0
+                        
+                        while counter < 3:
+                                position += 20
+                                servo.angle = position
+                                sleep(0.6)
+                                counter += 1
+                                print(position)
+
+                        counter = 0
+                        while counter < 6:
+                                position -= 20
+                                servo.angle = position
+                                sleep(0.6)
+                                counter += 1
+                        
+                        counter= 0
+                        while counter < 3:
+                                position += 20
+                                servo.angle = position
+                                sleep(0.6)
+                                counter += 1
+                servo.angle = 90
+        except KeyboardInterrupt:
+                print("Program stopped by user")
 
 # Main program loop
 try:
     while True:
-        angle = int(input("Enter angle (0 to 180): "))  # User input for angle
-        set_angle(angle)  # Set servo to entered angle
+        set_angle()
+        sleep(4)
 except KeyboardInterrupt:
     print("Program stopped by user")
 
