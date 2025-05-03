29266b38443677ef3cd1d761255d128099448e14
boufu <darylchee1234@gmail.com>
2025-05-03 22:53:48 +0800

    Update IR Sensor.py

--- IR Sensor.py
+++ IR Sensor.py
@@ -8,25 +8,29 @@ import RPi.GPIO as GPIO
 import time
 
 # initialize ir sensors to the physical pins
-IR_pin_1 = 8
-IR_pin_2 = 7
-IR_pin_3 = 9
-IR_pin_4 = 10
+IR_pin_1 = 21 # pins for the infrared sensors
+IR_pin_2 = 24
+IR_pin_3 = 26
+IR_pin_4 = 23
 print("IR pins initialized.")
 
 # set these pins as input
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(IR_pin_1, GPIO.IN) # setting infrared pins as input
+GPIO.setup(IR_pin_2, GPIO.IN)
+GPIO.setup(IR_pin_3, GPIO.IN)
+GPIO.setup(IR_pin_4, GPIO.IN)
 print("IR pins setup complete!")
-
-
-# create fire detection flag
-fire_detected = False # initial flag's state
-print("Fire detection flag initialized and set to FALSE")
 
 def test_IR():
 	IR_sensor_1 = GPIO.input(IR_pin_1)
+  IR_sensor_2 = GPIO.input(IR_pin_2)
+  IR_sensor_3 = GPIO.input(IR_pin_3)
+  IR_sensor_4 = GPIO.input(IR_pin_4)
 	print(f"IR input is {IR_sensor_1}")
+  time.sleep(1)
+  
+
 
 try:
   while True:
